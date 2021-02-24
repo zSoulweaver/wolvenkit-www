9b56360e5b6593788f0cad8a74f9195ada448de0
zSoulweaver <zSoulweaver@users.noreply.github.com>
2021-02-24 10:42:46 +0000

    deploy: 255f75cf13da4cc5f5acfe2cecf2e4ca75e96737

--- _nuxt/static/1613973170/blog/2021-02-17-website-and-co/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613973170",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog\u002F2021-02-17-website-and-co",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b12be68c"}}}}(null));
